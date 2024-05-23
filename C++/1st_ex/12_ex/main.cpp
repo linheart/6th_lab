03e63caeb618653d8dafd226638ba749b3e8927f
Alexei <leshamihaylenko@bk.ru>
2024-05-23 19:31:50 +0700

    C++

--- C++/1st_ex/12_ex/main.cpp
+++ C++/1st_ex/12_ex/main.cpp
@@ -1,43 +1,48 @@
 #include <iostream>
-#include <random>
 #include <vector>
 
 using namespace std;
 using vectors = vector<vector<int>>;
 
-#define N 5
-
 void out_matrix(vectors &);
-int gen_rand_num();
-void init(vectors &);
+void init(vectors &, int);
+void under_diag(vectors &);
+void above_diag(vectors &, int, int);
 
 int main() {
-  vectors matrix(N);
-  init(matrix);
+  int N;
+  cin >> N;
+  vectors matrix(N, vector<int>(N, 0));
+  init(matrix, N);
   out_matrix(matrix);
   return 0;
 }
 
 void out_matrix(vectors &matrix) {
   for (auto row : matrix) {
-    for (auto column : row)
-      cout << column << ' ';
+    for (auto column : row) {
+      if (column == 0)
+        cout << "000 ";
+      else
+        cout << column << ' ';
+    }
     cout << endl;
   }
 }
 
-int gen_rand_num() {
-  random_device rd;
-  mt19937 gen(rd());
-  uniform_int_distribution<> dis(1, 100);
-  return dis(gen);
+void under_diag(vectors &matrix, int i, int N) {
+  for (int row = i + 1; row < N; row++)
+    matrix[row][i] = 100 + i;
 }
 
-void init(vectors &matrix) {
-  int num = 10;
-  for (int i = 0; i < N; i++)
-    for (int j = 0; j < i + 1; j++) {
-      matrix[i].push_back(num);
-      num += 2;
-    }
+void above_diag(vectors &matrix, int i, int N) {
+  for (int row = 0; row < i; row++)
+    matrix[row][i] = 100 + N - 1 - i;
+}
+
+void init(vectors &matrix, int N) {
+  for (int i = 0, j = 0; i < N; i++, j++) {
+    under_diag(matrix, i, N);
+    above_diag(matrix, i, N);
+  }
 }
