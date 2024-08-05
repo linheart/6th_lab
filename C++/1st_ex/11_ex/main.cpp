39e866a85b4cd9975924ac70b3818b7718066571
Linheart <leshamihaylenko@bk.ru>
2024-08-05 17:00:30 +0700

    11_ex

--- C++/1st_ex/11_ex/main.cpp
+++ C++/1st_ex/11_ex/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using vectors = vector<vector<int>>;
 
 #define M 5
-#define N 5
+#define N 6
 
 void out_matrix(vectors &);
 int gen_rand_num();
@@ -16,47 +16,59 @@ bool is_prime(int);
 int find_max_prime(vectors &);
 
 int main() {
-  vectors matrix(M, vector<int>(N, 0));
+  int res = 1;
 
+  vectors matrix(M, vector<int>(N, 0));
   init(matrix);
   out_matrix(matrix);
-	cout << find_max_prime(matrix) << endl;
+  res = find_max_prime(matrix);
 
+  if (res) {
+    cout << res << endl;
+  } else {
+    cout << "There's no prime nums" << endl;
+  }
   return 0;
 }
 
 bool is_prime(int num) {
-  if (num <= 1 || num % 2 == 0 || num % 3 == 0)
+  if (num <= 1) {
     return false;
-  if (num <= 3)
+  }
+  if (num <= 3) {
     return true;
+  }
+  if (num % 2 == 0 || num % 3 == 0) {
+    return false;
+  }
 
   for (int i = 5; i < sqrt(num); i += 6)
-    if (num % i == 0 || num % (i + 2) == 0)
-      return false;
+    if (num % i == 0 || num % (i + 2) == 0) return false;
   return true;
 }
 
 int find_max_prime(vectors &matrix) {
-	vector<int> prime_nums(47, 0);
-	for (size_t i = 0; i < matrix.size(); i++)
-		for (size_t j = 0; j < matrix[i].size(); j++) {
-			if (is_prime(matrix[i][j]))
-				prime_nums[matrix[i][j]]++;
-		}
-	int max_count = *max_element(prime_nums.begin(), prime_nums.end());
+  vector<int> prime_nums(47, 0);
+  for (size_t i = 0; i < matrix.size(); i++)
+    for (size_t j = 0; j < matrix[i].size(); j++) {
+      if (is_prime(matrix[i][j])) prime_nums[matrix[i][j]]++;
+    }
 
-	int i = 0;
-	for (; prime_nums[i] != max_count; i++)
-		;
+  int max_count = *max_element(prime_nums.begin(), prime_nums.end());
+  auto it = find(prime_nums.begin(), prime_nums.end(), max_count);
 
-	return i;
+  if (it != prime_nums.end()) {
+    return it - prime_nums.begin();
+  }
+
+  return 0;
 }
 
 void out_matrix(vectors &matrix) {
   for (auto row : matrix) {
-    for (auto column : row)
+    for (auto column : row) {
       cout << column << ' ';
+    }
     cout << endl;
   }
 }
@@ -69,7 +81,9 @@ int gen_rand_num() {
 }
 
 void init(vectors &matrix) {
-  for (int i = 0; i < M; i++)
-    for (int j = 0; j < N; j++)
-			matrix[i][j] = gen_rand_num();
+  for (int i = 0; i < M; i++) {
+    for (int j = 0; j < N; j++) {
+      matrix[i][j] = gen_rand_num();
+    }
+  }
 }
